ec4466be58a0a666837b1db98a3fe721e9d281aa
M4dEggs <108707189+M4dEggs@users.noreply.github.com>
2022-10-22 08:42:30 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+function btn_test_1(){
+    console.log('button 1');
+    document.getElementById('s1').src = "image/x.png";
+    document.getElementById('s1').style.display = 'block';
+    document.getElementById('s1').style.top = 10 + "px";
+    document.getElementById('s1').style.width = 110+"px";
+    document.getElementById('s1').style.height = 110+"px";
+}
+
+function btn_test_2(){
+    console.log('button 1');
+    document.getElementById('s2').src = "image/x.png";
+    document.getElementById('s2').style.display = 'block';
+    document.getElementById('s2').style.top = 10 + "px";
+    document.getElementById('s2').style.width = 110+"px";
+    document.getElementById('s2').style.height = 110+"px";
+}
