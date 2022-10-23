58694a89e25e80316cf1aa366e9db47ba1c8e6fc
M4dEggs <108707189+M4dEggs@users.noreply.github.com>
2022-10-23 07:07:00 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,124 @@
+
+
+var game = 0;
+var turn = 1;
+
+if (game < 3){
+    function btn_test_1(){
+        console.log('button 1');
+        if (turn < 1){
+            document.getElementById('s1').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s1').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s1').style.display = 'block';
+    }
+    
+    function btn_test_2(){
+        console.log('button 2');
+        if (turn < 1){
+            document.getElementById('s2').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s2').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s2').style.display = 'block';
+
+    }
+
+    function btn_test_3(){
+        console.log('button 3');
+        if (turn < 1){
+            document.getElementById('s3').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s3').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s3').style.display = 'block';
+
+    }
+
+    function btn_test_4(){
+        console.log('button 4');
+        if (turn < 1){
+            document.getElementById('s4').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s4').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s4').style.display = 'block';
+    }
+    
+    function btn_test_5(){
+        console.log('button 5');
+        if (turn < 1){
+            document.getElementById('s5').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s5').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s5').style.display = 'block';
+
+    }
+
+    function btn_test_6(){
+        console.log('button 6');
+        if (turn < 1){
+            document.getElementById('s6').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s6').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s6').style.display = 'block';
+
+    }
+
+    function btn_test_7(){
+        console.log('button 7');
+        if (turn < 1){
+            document.getElementById('s7').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s7').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s7').style.display = 'block';
+    }
+    
+    function btn_test_8(){
+        console.log('button 8');
+        if (turn < 1){
+            document.getElementById('s8').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s8').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s8').style.display = 'block';
+
+    }
+
+    function btn_test_9(){
+        console.log('button 9');
+        if (turn < 1){
+            document.getElementById('s9').src = "image/x.png";
+            turn += 1;
+        }else {
+            document.getElementById('s9').src = "image/circle.png";
+            turn = 0;
+        }
+        document.getElementById('s9').style.display = 'block';
+
+    }
+}
+
+
+
+
